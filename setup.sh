702b398bfe9bd035973bb8bfa54bc1854ef738a6
Christopher George <cmgeorge@andrew.cmu.edu>
2020-02-03 12:07:55 -0500

    updated

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git clone --recursive https://github.com/NVIDIA/semantic-segmentation.git
+cd semantic-segmentation
+mkdir ./pretrained_model/
+pip install gdown
+gdown https://drive.google.com/uc?id=1P4kPaMY-SmQ3yPJQTJ7xMGAB_Su-1zTl
